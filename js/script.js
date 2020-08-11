6add4cb5eff4c70f2362e4d4aa83a72f7c18431e
nikolas2288 <67052680+nikolas2288@users.noreply.github.com>
2020-08-11 12:33:40 +0300

    1.0

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var mySwiper = new Swiper('.js-swiper-product', {
+    loop: true,
+    navigation: {
+        nextEl: '.slider--button--right',
+        prevEl: '.slider--button--left',
+    }
+})
+
+
